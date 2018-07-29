9756df76d4f5d8bc9ce675bd8c36481dc47a50b2
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-29 21:33:01 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => []
+      hero_friends => []
    },
    :capulet => {
       :patriarch => {
@@ -30,7 +30,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      heroine_friends => []
    }
   }
 
